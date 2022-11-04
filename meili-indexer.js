2bbd459ab7482834c26a4319f54371914b1a469f
Kristóf Kerti <kkerti@riseup.net>
2022-11-04 15:30:46 +0100

    KKERTI index documentation on commit

---
+++ meili-indexer.js
@@ -0,0 +1,69 @@
+import fs from 'fs';
+import {v4 as uuidv4} from 'uuid';
+import { dirname } from "path";
+import parseMD from 'parse-md';
+import { MeiliSearch } from 'meilisearch';
+import dotenv from 'dotenv';
+dotenv.config();
+
+let data = [];
+
+const meiliClient = new MeiliSearch({
+  host: process.env['MEILI_ADMIN_HOST'],
+  apiKey: process.env['MEILI_ADMIN_KEY'],
+  headers: {'Access-Control-Allow-Origin': '*'}
+})
+
+function readFilesSync(dirname){
+  fs.readdirSync(dirname).forEach((filename) => {
+
+    if(!fs.lstatSync(dirname + filename).isDirectory()) {
+
+      const fileContent = fs.readFileSync(dirname + filename, 'utf-8');
+
+      if(filename.endsWith('.md') || filename.endsWith('.mdx')) {
+
+        const { content, metadata } = parseMD(fileContent);
+
+        data.push({
+          key: uuidv4(),
+          path: generatePathName(dirname + filename, metadata.slug),
+          ...metadata,
+          body: content,
+        })
+      }
+
+    } else {
+      readFilesSync(dirname + filename + '/');
+    }
+
+  });
+}
+
+function generatePathName(filename, slug){
+  const split = filename.split('/docs');
+  let path = '';
+  // if the slug is present, use it instead of the file path
+  if(slug == undefined){
+    path = '/docs' + split[1].split('.')[0];
+  } else {
+    path = '/docs' + dirname(split[1]) + '/' + slug;
+  }
+  return path;
+}
+
+async function indexing(){
+      
+  try {
+    await meiliClient.index(process.env.MEILI_INDEX).delete();
+    meiliClient.index(process.env.MEILI_INDEX).addDocuments(data);
+    console.info('meilisearch is done');
+  } catch (error) {
+    console.error('meilisearch is not available', error);
+  }
+    
+}
+
+readFilesSync(process.env.PATH_TO_DOCUMENTS);
+
+indexing();
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,11 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures'
 
 import styles from './index.module.css'
 
+import { InstantSearch, SearchBox, Hits, Highlight } from 'react-instantsearch-dom';
+import { instantMeiliSearch } from '@meilisearch/instant-meilisearch';
+
+const searchClient = instantMeiliSearch('https://meilisearch-1n6r.onrender.com/', 'securepassword');
+
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext()
   return (
@@ -27,13 +32,23 @@ function HomepageHeader() {
   )
 }
 
+const Hit = ({ hit }) => <Highlight attribute="name" hit={hit} />;
+
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext()
   return (
+    
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />"
     >
+      <InstantSearch
+        indexName="grid-documentation"
+        searchClient={searchClient}
+      >
+      <SearchBox />
+      <Hits hitComponent={Hit} />
+    </InstantSearch>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
