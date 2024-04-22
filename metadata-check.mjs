67d1b9a9a967b2b49963d8c53d4ff75e4e297db4
Bernadett <bettyblack23@gmail.com>
2024-04-22 11:40:22 +0200

    betty: delete console.log

--- metadata-check.mjs
+++ metadata-check.mjs
@@ -43,6 +43,4 @@ if (filesWithError.length > 0) {
   console.error(
     `Please fix the metadata issue(s). Always include the following meta data fields: 'description', 'title', 'pageImg', 'slug'.`
   );
-
-  console.log("testt");
 }
